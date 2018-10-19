2219f62f4fbec64fc34b3c32901754975651cb27
Ian Pilipski <ipilipski@gsngames.com>
2018-10-19 19:07:26 -0400

    Initial commit for testing

---
+++ Assets/Editor/CommandLineBuild.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
+public class CommandLineBuild {
+	[MenuItem("GSN/Build/Build iOS")]
+	public static void iOSBuild ()
+	{
+		BuildPlayerOptions options = new BuildPlayerOptions();
+		options.targetGroup = BuildTargetGroup.iOS;
+		options.target = BuildTarget.iOS;
+		options.scenes = new string[] { "Assets/Scenes/Main.unity" };
+		options.locationPathName = "Builds/iOS";
+		
+		BuildPipeline.BuildPlayer(options);
+	}
+}
